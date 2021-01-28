9e21a8d5f03dec6a0c2185f32ce11c73d5493e2e
Sergo Gabunia <sergogabunia@gmail.com>
2021-01-29 01:16:08 +0400

    submitted project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FormInput from "./components/FormInput";
+import Todo from "./components/Todo";
 
 function App() {
+  const [todo, setTodo] = useState({ todo: "", id: 0 });
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState(false);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    if (todo.todo) {
+      setTodos([...todos, todo]);
+      setTodo({ todo: "", id: 0 });
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+  const removeTodo = (id) => {
+    const newToDos = todos.filter((todo) => todo.id !== id);
+    setTodos(newToDos);
+  };
+
+  const handleChange = (e) => {
+    setTodo({ todo: e.target.value, id: Date.now() });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todos">
+        <h2>You have {todos.length} Todos</h2>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo.todo}
+            handleRemove={() => removeTodo(todo.id)}
+          />
+        ))}
+        <FormInput
+          handleInputChange={handleChange}
+          handleAdd={addTodo}
+          value={todo.todo}
+          error={error}
+        />
+      </div>
     </div>
   );
 }
